52605da0b756721d9e51b783281fc433aa9780f3
Joacim Olofsson <Joacim Olofsson>
2016-10-05 01:15:16 +0200

    Cleaned out some transpiled files and other garbage

--- dev/boot.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var browser_1 = require('angular2/platform/browser');
-var app_component_1 = require('./appComponent.ts');
-browser_1.bootstrap(app_component_1.AppComponent);
-//# sourceMappingURL=boot.js.map
